f5c63067449cc48cb8d2aa5f1848711032fa9aae
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-02 17:48:58 +0530

    resolve error

--- app/api/auth/checkAuth/route.ts
+++ app/api/auth/checkAuth/route.ts
@@ -1,8 +1,18 @@
 import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
+import jwt from "jsonwebtoken" ;
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"../../../.env"
+})
+
+console.log(process.env.SECRET)
 
 export async function GET(req:Request){
-    console.log("working")
+    const token = (await cookies()).get("jwt") ;
+
+    // const answer = jwt.verify(token?.value,)
 
     return NextResponse.json({message:"workint"})
 }
