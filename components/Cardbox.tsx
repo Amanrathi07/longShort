6188b2cb5f606838bac3e716aff9e38c82bb78f7
Amanrathi07 <amanrathi4321@gmail.com>
2026-01-06 02:41:06 +0530

    refectior

--- components/Cardbox.tsx
+++ components/Cardbox.tsx
@@ -1,19 +1,30 @@
 import prismaClient from "@/lib/client"
 import Card from "./Card";
 import { cookies } from "next/headers";
+import  jwt, { JwtPayload }  from "jsonwebtoken"
+import dotenv from "dotenv" ;
+
+dotenv.config({
+    path: "../.env"
+})
+
 
 async function allLinks() {
-    const token = (await cookies()).get("jwt");
-    
-    const responce =  await prismaClient.link.findMany()
-    return responce ;
+    const token = (await cookies()).get("jwt")
+    if(!token) return
+    const jwtToken = jwt.verify(token?.value,process.env.SECRET!);
+    if(!jwtToken) return 
+    const dbResponce = await prismaClient.link.findMany({where:{userId : jwtToken as string}})
+
+    return dbResponce
 }
 
 
 export default async function Cardbox() {
   
-  const data = await allLinks();
 
+  const data = await allLinks();
+  console.log(data)
   if (!data || data.length === 0) {
     return (
       <div className="flex justify-center py-20 text-gray-400">
