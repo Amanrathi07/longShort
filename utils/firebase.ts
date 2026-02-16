2664a3003727845437fbb77ace10fc21e9dc10e2
Amanrathi07 <amanrathi4321@gmail.com>
2026-02-16 22:27:32 +0530

    st

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
 
 import { getAuth, GoogleAuthProvider, signInWithPopup }from"firebase/auth"
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+        const firebaseConfig = {
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
